b6dc4371a7c0a3bf9dea60f8ab7fdd44b9512b67
born2slayy77 <born2slayy77@gmail.com>
2024-08-03 02:45:57 +0900

    start.sh change for nohup

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,7 +15,8 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE &
+nohup java -jar ~/springapp/spring-webapp.jar > nohup.out 2>&1 &
+
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
